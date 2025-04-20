064da4aa9e623cbec1998cb58a6a4d991f6f764e
14elias <ellamebrahtom1995@gmail.com>
2025-04-21 01:56:02 +0300

    updated

--- storefront/urls.py
+++ storefront/urls.py
@@ -1,9 +1,14 @@
 from rest_framework_nested.routers import DefaultRouter, NestedDefaultRouter
 from django.urls import path
-from.views import ProductViewset,CategoryViewset
+from.views import ProductViewset,CategoryViewset, ReveiwViewset
 
+#main router
 router = DefaultRouter()
 router.register(r'product', ProductViewset, basename='product')
 router.register(r'category', CategoryViewset, basename='category')
 
-urlpatterns = router.urls
+#default router
+product_router=NestedDefaultRouter(router, r'product', lookup='product')
+product_router.register(r'review',ReveiwViewset,basename='product-review')
+
+urlpatterns = router.urls + product_router.urls
--- storefront/views.py
+++ storefront/views.py
@@ -2,16 +2,32 @@ from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
 from .permissions import IsAdminOrReadOnly
-from .models import Category, Product
-from .serializers import ProductSerializer,CategorySerializer
+from .models import Category, Product, Reveiw
+from .serializers import ProductSerializer,CategorySerializer, ReveiwSerializer
+from.pagination import DefaultPagination
 
 class ProductViewset(ModelViewSet):
-    queryset = Product.objects.all()
     serializer_class = ProductSerializer
     permission_classes = [IsAdminOrReadOnly]
+    pagination_class=DefaultPagination
+
+    def get_queryset(self):
+        queryset = Product.objects.all()
+        category_id = self.request.query_params.get('category_id')
+        if category_id is not None:
+            return  queryset.filter(category_id=category_id)
+        return queryset
 
 class CategoryViewset(ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
     permission_classes = [IsAdminOrReadOnly]
 
+class ReveiwViewset(ModelViewSet):
+    serializer_class = ReveiwSerializer
+
+    def get_queryset(self):
+        return Reveiw.objects.filter(product_id=self.kwargs['product_pk'])
+    
+    def get_serializer_context(self):
+        return {'product_id':self.kwargs['product_pk'],'request':self.request}
