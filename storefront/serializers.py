f14679a35ee5f0b6826f8700bdbcc044be664f77
14elias <ellamebrahtom1995@gmail.com>
2025-04-21 01:55:43 +0300

    ...

--- storefront/serializers.py
+++ storefront/serializers.py
@@ -1,5 +1,5 @@
-from rest_framework.serializers import ModelSerializer
-from .models import Product,Category
+from rest_framework.serializers import ModelSerializer,SerializerMethodField
+from .models import Product,Category, Reveiw
 
 class ProductSerializer(ModelSerializer):
     class Meta:
@@ -17,3 +17,21 @@ class CategorySerializer(ModelSerializer):
         extra_kwargs={
             'created_at':{'read_only':True}
         }
+
+class ReveiwSerializer(ModelSerializer):
+    user=SerializerMethodField(read_only=True)
+    class Meta:
+        model = Reveiw
+        fields = ['id','name','product','created_at','description','user']
+        extra_kwargs={
+            'product':{'read_only':True}
+        }
+    
+    def get_user(self,obj):
+        return obj.user.first_name
+    
+    def create(self, validated_data):
+        user = self.context['request'].user
+        product_id = self.context['product_id']
+        review = Reveiw.objects.create(user=user,product_id=product_id,**validated_data)
+        return review
