5099e4b91c28cc7083e224ac035b716e8523bd31
14elias <ellamebrahtom1995@gmail.com>
2025-04-21 17:35:45 +0300

    ..

--- storefront/urls.py
+++ storefront/urls.py
@@ -1,15 +1,18 @@
 from rest_framework_nested.routers import DefaultRouter, NestedDefaultRouter
 from django.urls import path
-from.views import CartViewset, ProductViewset,CategoryViewset, ReveiwViewset
+from.views import CartItemViewset, CartViewset, ProductViewset,CategoryViewset, ReveiwViewset
 
 #main router
 router = DefaultRouter()
 router.register(r'product', ProductViewset, basename='product')
 router.register(r'category', CategoryViewset, basename='category')
-router.register(r'cart',CartViewset, basename='cart')
+router.register(r'cart', CartViewset, basename='cart')
 
 #default router
 product_router=NestedDefaultRouter(router, r'product', lookup='product')
 product_router.register(r'review',ReveiwViewset,basename='product-review')
 
-urlpatterns = router.urls + product_router.urls
+cart_router = NestedDefaultRouter(router, r'cart', lookup='cart')
+cart_router.register(r'items', CartItemViewset, basename ='cart_items')
+
+urlpatterns = router.urls + product_router.urls + cart_router.urls
