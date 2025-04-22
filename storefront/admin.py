8bb7d96fe3bf7ace844f30661b72398aa29dcd5d
14elias <ellamebrahtom1995@gmail.com>
2025-04-22 17:38:39 +0300

    admin update

--- storefront/admin.py
+++ storefront/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
-from .models import Product,Category,Reveiw,CartItem
+from .models import Cart, Order, OrderItem, Product,Category,Reveiw,CartItem
 # Register your models here.
 admin.site.register(Product)
 admin.site.register(Category)
 admin.site.register(Reveiw)
 admin.site.register(CartItem)
+admin.site.register(OrderItem)
+admin.site.register(Order)
+
+class CartAdmin(admin.ModelAdmin):
+    list_display = ['id','created_at','user__first_name']
+
+admin.site.register(Cart,CartAdmin)
