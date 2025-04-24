8e29f450ee3f18400b3112e7cdb01d615407b4fb
14elias <ellamebrahtom1995@gmail.com>
2025-04-24 17:30:31 +0300

    update home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
-
+import Hero from '../components/Hero'
+import Latest from '../components/Latest'
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <Hero/>
+      <Latest/>
+    </div>
   )
 }
 
