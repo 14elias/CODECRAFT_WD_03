4c794ac0d7d18315bdb0e76c1a11ab574e6e6774
14elias <ellamebrahtom1995@gmail.com>
2025-04-24 17:26:24 +0300

    update

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
+import { useState } from 'react';
+import { Link, NavLink } from 'react-router-dom'
 import { CiSearch } from "react-icons/ci";
 import { FaUser } from 'react-icons/fa';
+import { FaBars } from 'react-icons/fa';
+import { FaShoppingCart } from 'react-icons/fa';
 
 const Navbar = () => {
+    const [visible,setVisible] = useState(false)
+
+    const handleClick = ()=>{
+        setVisible(!visible)
+    }
+
   return (
     <div className='flex items-center justify-between py-5 font-medium'>
         <p className='w-36'>MyEcom</p>
@@ -32,7 +41,7 @@ const Navbar = () => {
             <div className='group relatives'>
                 <FaUser className='w-5 cursor-pointer'/>
                 <div className='group-hover:block hidden absolute dropdown-menu right-0 pt-4'>
-                    <div className='flex flex-col gap-2 w-36 py-3 bg-slate-100 text-gray-500 rounded'>
+                    <div className='flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded'>
                         <p className='cursor-pointer hover:text-black'>My Profile</p>
                         <p className='cursor-pointer hover:text-black'>Orders</p>
                         <p className='cursor-pointer hover:text-black'>Logout</p>
@@ -40,6 +49,24 @@ const Navbar = () => {
                     </div>
 
                 </div>
+
+            </div>
+            <Link to='/cart' className='relative'>
+                <FaShoppingCart className='w-5 min-w-5'/>
+                <p className='absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]'>10</p>
+            </Link>
+            <FaBars className='w-5 cursor-pointer sm:hidden' onClick={handleClick}/>
+        </div>
+        {/* side bar menu for small screens */}
+        <div className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all ${visible? 'w-full':'w-0'}`}>
+            <div className='flex flex-col text-gray-600'>
+                <div className='flex item-center gap-4 p-3 cursor-pointer' onClick={handleClick}>
+                    <p>Back</p>
+                </div>
+                <NavLink to='/' className='py-2 pi-6 border' onClick={handleClick}>Home</NavLink>
+                <NavLink to='/collection' className='py-2 pi-6 border' onClick={handleClick}>Collection</NavLink>
+                <NavLink to='/about' className='py-2 pi-6 border' onClick={handleClick}>About</NavLink>
+                <NavLink to='/contact' className='py-2 pi-6 border' onClick={handleClick}>Contact</NavLink>
 
             </div>
 
